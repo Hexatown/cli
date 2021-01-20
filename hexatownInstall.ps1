f8d42aac8c5e3a799651f759f6a7bab42eb5571a
Niels Gregers Johansen <niels@timeboxer.dk>
2021-01-20 09:57:51 +0100

    added demo to installation

--- hexatownInstall.ps1
+++ hexatownInstall.ps1
@@ -1,4 +1,4 @@
-Write-Host "Installing  " -ForegroundColor Green
+Write-Host "Installing Hexatown " -ForegroundColor Green
 
 $environmentPath = ([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::CommonApplicationData)) 
 
@@ -29,4 +29,18 @@ Copy-Item "$PSScriptRoot\package.json" -Destination $destdir
 Copy-Item "$PSScriptRoot\src" -Destination $destdir
 
 Push-Location $destdir
+Write-Host "****************************************************************" -ForegroundColor White -BackgroundColor Black
+Write-Host " Running DEMO                                                   " -ForegroundColor White -BackgroundColor Black
+Write-Host "****************************************************************" -ForegroundColor White -BackgroundColor Black
+Write-Host " "                                                              -ForegroundColor White -BackgroundColor Black
+Write-Host " If this is the first time you install Hexatown, you will be    " -ForegroundColor Yellow -BackgroundColor Black
+Write-Host " prompted to consent to Hexatown getting access to your account " -ForegroundColor Yellow -BackgroundColor Black
+Write-Host " the code that you need to use for sign in is on the clipboard  " -ForegroundColor Yellow -BackgroundColor Black
+Write-Host " "                                                              -ForegroundColor White -BackgroundColor Black
+Write-Host "**************************************************************" -ForegroundColor White -BackgroundColor Black
+Write-Host "Press Enter to continue ..." -ForegroundColor Green
+read-host 
 . "./src/jobs/powershell/myteams.ps1"
+
+Write-Host "Press Enter to close ..." -ForegroundColor Green
+read-host 
--- src/jobs/powershell/myteams.ps1
+++ src/jobs/powershell/myteams.ps1
@@ -1,8 +1,8 @@
-. "$PSScriptRoot\.hexatown.com.ps1"                                         # Load the Hexatown framework
-$hexatown = Init $MyInvocation  $false $true                                # Stop the framework
-$myTeams = LIST $hexatown $userAPI.teams                                    # Read the Teams of the delegate user
-foreach ($team in $myTeams | Sort-Object -Property displayName  )           # Sort teams by display name and run the code block
-{                                                                           # Start code block
-    Write-Host $team.displayName                                            # Write the value of a team to the host
-}                                                                           # End code block
-Done $hexatown                                                              # Stop the framework       
+. "$PSScriptRoot\.hexatown.com.ps1"                # Load the Hexatown framework
+$hexatown = Start-Hexatown  $myInvocation          # Start the framework
+$myTeams = List $hexatown my teams by displayName  # Read the Teams of the delegate user sorted by name
+foreach ($team in $myTeams )                       # Repeat code for each team
+{                                                  # Start code block
+    Write-Host $team.displayName                   # Write the value of a team to the host
+}                                                  # End code block
+Stop-Hexatown $hexatown                            # Stop the framework       
